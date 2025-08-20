ff53ca4a925674093eecb1ecd3309259afbd807c
Jackelin <jackelinvasquezguzman@gmail.com>
2025-08-20 09:23:51 -0600

    Comentario tipo de atributo

--- Actividad_20.py
+++ Actividad_20.py
@@ -1,9 +1,16 @@
 class Empleado:
     def __init__(self,codigo,nombre,puesto,salario):
-        self.codigo= codigo
+        self.__codigo= codigo
+        #El codigo debe ser privado porque es un dato que identifica al empleado y si es modificado
+        #puede causar que los datos del empleado tambien se vean afectados o que datos de otros empleados
+        #sean sobre escritos, por ello para mayor seguridad no debe tener acceso a dicho dato.
         self.nombre=nombre
-        self.puesto=puesto
-        self.salario= salario
+        #El nombre es un dato que se puede consultar en cualquier parte del codigo, no deberia existir
+        #problemas al dejarlo publico.
+        self._puesto=puesto
+        self._salario= salario
+        #el salario y puesto pueden cambiar, ya que el empleado puede tener un aumento o un acenso,
+        #por ello son datos protegidos, pueden modificarse dentro de la clase por medio de metodos.
 
 class Departamento:
     def __init__(self,codigo_interno,nombre):
